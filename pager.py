5f17c539664646a4317ac831708903d93b27a668
Dániel Bene <danielbene@protonmail.com>
2021-07-28 20:30:19 +0200

    Add cache control, working on blockage

--- pager.py
+++ pager.py
@@ -2,6 +2,7 @@ import os
 from time import sleep
 from bottle import request, route, run, static_file
 from multiprocessing import Process
+from threading import Thread
 
 
 @route('/')
@@ -11,30 +12,38 @@ def main():
 
 @route('/shutdown')
 def shutdown():
-    Process(target=proc_task(request.path)).start()
+    # Process(target=proc_task(request.path)).start()
+    Thread(target=proc_task(request.path)).start()
     return server_static('shutdown.html')
 
 
 @route('/reboot')
 def reboot():
-    Process(target=proc_task(request.path)).start()
+    # Process(target=proc_task(request.path)).start()
+    # new Thread still blocking the main execution
+    t = Thread(target=proc_task('/reboot'))
+    t.daemon = False
+    t.start()
+    print('started')
     return server_static('reboot.html')
 
 
 @route('/<filename>')
 def server_static(filename):
-    return static_file(filename,
-                       root=os.path.dirname(__file__) + '/static')
+    static = static_file(filename,
+                         root=os.path.dirname(__file__) + '/static')
+    static.add_header('Cache-Control', 'no-cache, no-store, must-revalidate')
+    return static
 
 
 def proc_task(mode):
     sleep(5)
     if (mode == '/shutdown'):
         print('shutdown')
-        os.system('shutdown now')
+        # os.system('/sbin/shutdown now')
     else:
         print('reboot')
-        os.system('reboot now')
+        # os.system('/sbin/reboot now')
 
 
 localip = os.popen('hostname -I').read().split(' ')[0]
